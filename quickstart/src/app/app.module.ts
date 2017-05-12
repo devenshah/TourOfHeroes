4442e6c9ee4de378e88e7b571f425e1937e770e3
Deven Shah <deven.shah@capita.co.uk>
2017-05-12 08:16:36 +0100

    - Added in memory API Service - Added Add Update Delete functions

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-//import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent }  from './app.component';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
+import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroListComponent } from './hero-list.component';
 import { CrisisListComponent } from './crisis-list.component';
 import { PageNotFoundComponent } from './not-found.component';
 import { DashboardComponent } from './dashboard.component';
-
 import { HeroService } from './hero.service';
-
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
